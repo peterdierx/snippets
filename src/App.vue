10e79f76f6946086cd95141379b374e283765795
Peter Dierx <peterdierx@mac.com>
2019-03-23 14:34:26 +0100

    AppMenu

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <div>
     <AppHeader/>
+    <AppMenu/>
   </div>
 </template>
 
 <script>
   import AppHeader from '@/components/AppHeader'
+  import AppMenu   from '@/components/AppMenu'
 
   export default {
     name: 'App',
-    components: { AppHeader }
+    components: { AppHeader, AppMenu }
   }
 </script>
---
+++ src/components/AppMenu.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class='section' style='margin-top: -25px;'>
+    <div class='container'>
+      <div class='box'>
+        <div class='level'>
+          <div class='field is-grouped'>
+            <p class='control'>
+              <button class='button is-primary'>Snippets</button>
+            </p>
+            <p class='control'>
+              <button class='button is-link'>New</button>
+            </p>
+            <p class='control'>
+              <input class='input' placeholder='Search...'>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+  export default {
+    name: 'AppMenu'
+  }
+</script>
