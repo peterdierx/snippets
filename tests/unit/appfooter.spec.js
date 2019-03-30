16ec66f973d90a8a1c073a7af4ab617f94162d91
Peter Dierx <peterdierx@mac.com>
2019-03-30 17:31:26 +0100

    cleanup

--- tests/unit/appfooter.spec.js
+++ tests/unit/appfooter.spec.js
@@ -1,15 +1,15 @@
-import { mount } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
 import AppFooter from '@/components/AppFooter.vue'
 
 describe( 'AppFooter Component', () => {
 
   it( 'renders footer text', () => {
-    const wrapper = mount( AppFooter )
+    const wrapper = shallowMount( AppFooter )
     expect( wrapper.text() ).toMatch( 'Footer' )
   })
 
   it( 'renders h3 with footer', () => {
-    const wrapper = mount( AppFooter )
+    const wrapper = shallowMount( AppFooter )
     expect( wrapper.find( 'h3' ).text() ).toEqual( 'Footer' )
   })
 })
--- tests/unit/appheader.spec.js
+++ tests/unit/appheader.spec.js
@@ -1,4 +1,4 @@
-import { mount } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
 import AppHeader from '@/components/AppHeader.vue'
 
 describe( 'AppHeader Component', () => {
@@ -7,22 +7,22 @@ describe( 'AppHeader Component', () => {
   const subtitle = 'Peter Dierx - 2019'
 
   it( 'has a title', () => {
-    const wrapper = mount( AppHeader )
+    const wrapper = shallowMount( AppHeader )
     expect( wrapper.text() ).toMatch( title )
   })
 
   it( 'has a subtitle', () => {
-    const wrapper = mount( AppHeader )
+    const wrapper = shallowMount( AppHeader )
     expect( wrapper.text() ).toMatch( subtitle )
   })
   
   it( 'renders h1 with title', () => {
-    const wrapper = mount( AppHeader )
+    const wrapper = shallowMount( AppHeader )
     expect( wrapper.find( 'h1' ).text() ).toEqual( title )
   })
 
   it( 'renders h2 with subtitle', () => {
-    const wrapper = mount( AppHeader )
+    const wrapper = shallowMount( AppHeader )
     expect( wrapper.find( 'h2' ).text() ).toEqual( subtitle )
   })
 
